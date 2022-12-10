d861a8713fccd233255f2defd812c0eefd260dd7
nhatminh287 <nhatminhpro2001@gmail.com>
2022-12-10 11:15:29 +0700

    Phan Quyen

--- init db script/Privilege.sql
+++ init db script/Privilege.sql
@@ -2,4 +2,22 @@
 use ESHOPPING
 go
 
---viết tạo và phân quyền vào đây
+-- Phân hệ khách hàng
+create login KhachHang with PASSWORD = 'mk1234'
+create user KhachHang for login KhachHang
+grant select on ThucDon to KhachHang
+grant select on Mon to KhachHang
+grant select, insert, delete on ChiTietDonHang to KhachHang
+grant select, insert, delete on ChiTietGioHang to KhachHang
+grant insert, update on DonHang to KhachHang
+grant select on Quan to KhachHang
+grant select on ChiNhanh to KhachHang
+grant select, update, insert on KhachHang to KhachHang
+grant insert, update on TaiKhoan to KhachHang
+-- Phân hệ tài xế
+create login TaiXe with PASSWORD = 'mk12345'
+create user TaiXe for login TaiXe
+
+grant select, update on DonHang to TaiXe
+grant insert, update on TaiKhoan to TaiXe
+grant select, update, insert to TaiXe
